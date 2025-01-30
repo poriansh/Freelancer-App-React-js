873236146e5e32d6338040ce14970c68d761ac5c
poria neyshaboriyan <poria.nsh@gmail.com>
2025-01-30 16:30:11 +0330

    update project

--- front-end/src/features/Authentication/UserAvatar.jsx
+++ front-end/src/features/Authentication/UserAvatar.jsx
@@ -1,14 +1,12 @@
 
 function UserAvatar({user}) {
   return (
-    <div className="flex gap-3 items-center">
+    <div>
       <span className="font-Vazir text-secondary-900 text-sm hidden sm:block">
         {`  سلام `}
         {user?.name}
       </span>
-      <div className="w-7 h-7 overflow-hidden rounded-full">
-        <img className="w-7 h-7" src="../../../public/img/user.jpg" alt="user-avatar" />
-      </div>
+     
     </div>
   );
 }
